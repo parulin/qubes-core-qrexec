bb968196d27eb0529c0125ac0bdd6172a01dfc5d
parulin <161326115+parulin@users.noreply.github.com>
2026-07-15 15:05:42 +0000

    Add intersphinx mappings and local inventories
    Duplicated from qubes-os repo

--- doc/conf.py
+++ doc/conf.py
@@ -9,6 +9,7 @@
 # -- Path setup --------------------------------------------------------------
 
 import os
+import re
 import subprocess
 import sys
 import time
@@ -192,6 +193,29 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
-    "python": ("https://docs.python.org/", None),
-    "qubes": ("http://dev.qubes-os.org/projects/core-admin/en/latest/", None),
+    'python': ('http://docs.python.org/', None),
+    'qubes-doc': ('https://doc.qubes-os.org/en/latest/', None),
+    'core-admin': ('https://dev.qubes-os.org/projects/core-admin/en/latest/', None),
+    'core-admin-client': ('https://dev.qubes-os.org/projects/core-admin-client/en/latest/', None),
 }
+
+if limit := os.environ.get("QUBES_DOC_LOCAL_INTERSPHINX_CACHE_LIMIT"):
+    intersphinx_cache_limit = int(limit)
+
+if os.environ.get("QUBES_DOC_LOCAL_INTERSPHINX", "").lower() in ("true", "on", "1"):
+    local_pattern = os.environ.get(
+        "QUBES_DOC_LOCAL_INTERSPHINX_PATTERN", "../../{path}/_build/html"
+    )
+    for repo, (target, inventory) in intersphinx_mapping.items():
+        if re.match(r'https://(dev|doc)\.qubes-os.org/', target):
+            variable_name = "QUBES_DOC_LOCAL_INTERSPHINX_TARGET_{}".format(repo.replace('-', '_').upper())
+            new_target = os.environ.get(variable_name)
+
+            if not new_target:
+                if repo == "qubes-doc":
+                    path = repo
+                else:
+                    path = "qubes-{name}/doc".format(name=repo.removeprefix('qubes-'))
+                new_target = local_pattern.format(path=path)
+
+            intersphinx_mapping[repo] = (new_target, inventory)
